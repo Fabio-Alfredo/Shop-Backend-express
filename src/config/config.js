b4f0ce7039f24ead5f34b66250d79b0e697c825b
Fabio-Alfredo <00210121@uca.edu.sv>
2024-12-25 23:42:10 -0600

    refactor: enviroment for producction and develop

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { DATABASE, DB_USER, DB_PASSWORD, DB_HOST, PORT, DB_DIALECT } =
+const { PORT, DATABASE, DB_USER, DB_PASSWORD, DB_HOST, DB_DIALECT, PASS_SALT } =
   process.env;
 
 const validEnv = (env, name) => {
@@ -9,12 +9,23 @@ const validEnv = (env, name) => {
 };
 
 const config = {
-  port: validEnv(PORT, "PORT"),
-  database: validEnv(DATABASE, "DATABASE"),
-  db_user: validEnv(DB_USER, "DB USER"),
-  db_pass: validEnv(DB_PASSWORD, "DB PASSWORD"),
-  db_host: validEnv(DB_HOST, "HOST"),
-  db_dialect: validEnv(DB_DIALECT, "DB_DIALECT"),
+  development: {
+    username: validEnv(DB_USER, "DB_USER"),
+    password: validEnv(DB_PASSWORD, "DB_PASSWORD"),
+    database: validEnv(DATABASE, "DATABASE"),
+    socketPath: validEnv(DB_HOST, "DB_HOST"),
+    dialect: validEnv(DB_DIALECT, "DB_DIALECT"), 
+    salt: validEnv(PASS_SALT, "SALT ENCRYPT"),
+  },
+  production: {
+    port: validEnv(PORT, "PORT"),
+    database: validEnv(DATABASE, "DATABASE"),
+    db_user: validEnv(DB_USER, "DB USER"),
+    db_pass: validEnv(DB_PASSWORD, "DB PASSWORD"),
+    db_host: validEnv(DB_HOST, "HOST"),
+    db_dialect: validEnv(DB_DIALECT, "DB_DIALECT"),
+    salt: validEnv(PASS_SALT, "SALT ENCRYPT"),
+  },
 };
 
 module.exports = config;
