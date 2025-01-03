4c85948d9a69aa559ed0391d94518a12983f349b
Fabio-Alfredo <00210121@uca.edu.sv>
2025-01-03 11:52:00 -0600

    add: validate errors for create user

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -4,6 +4,9 @@ const UserCodes = require("../utils/errorsCodes/user.codes");
 
 const createUser = async (user) => {
   try {
+    const existUser = userRepository.existUser(user.email);
+    if (existUser)
+      throw new ServiceError("Email already in use", UserCodes.ALREADY_EXISTS);
     const newUser = await userRepository.create(user);
     return newUser;
   } catch (e) {
