8e400d65408a2a1113b3c763cbfdbb768dd66f2f
Fabio-Alfredo <00210121@uca.edu.sv>
2025-02-19 21:19:22 -0600

    add: new repositories for find and update information in payments :sparkles:

--- src/repositories/ paymet.repository.js
+++ src/repositories/ paymet.repository.js
@@ -10,7 +10,27 @@ const create = async (payment, t) => {
   return newPayment;
 };
 
+const findById = async (id) => {
+  const payment = await Payment.findByPk(id, {
+    include: {
+      model: Order,
+      through: { attributes: [] },
+    },
+  });
+  return payment;
+};
+
+const update = async (payment, id, t) => {
+  const updatedPayment = await Payment.update(payment, {
+    where: { id },
+    transaction: t,
+  });
+  return updatedPayment;
+}
+
 module.exports = {
   create,
-    startTransaction,
+  startTransaction,
+  findById,
+  update,
 };
