e92d99c1972d65ab312140ae267ce53e411dc6d9
Fabio-Alfredo <00210121@uca.edu.sv>
2025-02-21 11:53:06 -0600

    add: new controller for find all roles

---
+++ src/controllers/roles.controller.js
@@ -0,0 +1,19 @@
+const createHttpError = require('http-errors');
+const ServiceError = require('../utils/errors/service.error');
+const roleService = require('../services/role.service');
+const responseHandler = require('../handlers/response.handler');
+
+const findAllRoles = async (req, res, next) => {
+  try {
+    const roles = await roleService.findAll();
+    responseHandler(res, 200, 'success', roles);
+  } catch (e) {
+    switch (e.code) {
+      case ServiceError.NOT_FOUND:
+        next(createHttpError(404, e.message));
+        break;
+      default:
+        next(e);
+    }
+  }
+};
