0554df2e4d4073667e38d588f8d595a0ad21acb6
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-12 18:20:39 -0600

    add(app.js): usage middleware for upload files in body :sparkles:

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const Routes = require("./src/routes/index.route");
 const errorHandler = require("./src/handlers/error.handler");
+const fileUpload = require("express-fileupload");
 
 // Crear la aplicación de Express
 const app = express();
@@ -9,6 +10,7 @@ const app = express();
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
 app.use(Routes);
 app.use(errorHandler);
 
