ad612eb380ee9799181aed2deefcc0bd52cb787d
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-12 18:54:17 -0600

    add(product_images.model): add new model for save images :sparkles:

---
+++ src/domain/models/product_images.model.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, DataTypes) => {
+  const Product_images = sequelize.define(
+    "Product_images",
+    {
+      id: {
+        allowNull: false,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      url: {
+        type: DataTypes.STRING,
+      },
+      product_id: {
+        type: DataTypes.UUID,
+      },
+    },
+    {
+      tableName: "products_images",
+      timestamps: true,
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    }
+  );
+
+  Product_images.associate = (models) => {
+    Product_images.belongsTo(models.Product, {
+      foreignKey: "product_id",
+      as: "product",
+    });
+  };
+
+  return Product_images;
+};
