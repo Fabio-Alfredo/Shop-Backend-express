a788f843fe884580dcc1935dcd11a8bcc93791af
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-12 18:53:31 -0600

    add(uploadImage.handler): add validation for upload images :sparkles:

--- src/handlers/uploadImage.handler.js
+++ src/handlers/uploadImage.handler.js
@@ -2,20 +2,27 @@ const admin = require("../configs/firebase.config");
 
 const bucket = admin.storage().bucket();
 
-const uploadImage = async (image, path) => {
-  const remotePath = `${path}/${image.name}${Date.now()}`;
-  const file = bucket.file(remotePath);
-  await file.save(image.data, {
-    metadata: {
-      contentType: image.mimetype,
-    },
-  });
+const uploadImage = async (images, path) => {
+  const files = Array.isArray(images) ? images : [images];
 
-  const [url] = await file.getSignedUrl({
-    action: "read",
-    expires: Date.now() + 24 * 60 * 60 * 1000, // 24 hours
-  });
-  return url;
+  const urls = await Promise.all(
+    files.map(async (image) => {
+      const remotePath = `${path}/${image.name}${Date.now()}`;
+      const file = bucket.file(remotePath);
+      await file.save(image.data, {
+        metadata: {
+          contentType: image.mimetype,
+        },
+      });
+      const [url] = await file.getSignedUrl({
+        action: "read",
+        expires: Date.now() + 24 * 60 * 60 * 1000, // 24 hours
+      });
+      return url;
+    })
+  );
+
+  return urls;
 };
 
 module.exports = uploadImage;
